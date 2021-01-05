8511b75c109c2e4e16cadd98737a789c63253c10
Zohaib Hassan <zohaibhassan@Zohaibs-MacBook-Pro.local>
2021-01-05 10:00:00 +0500

    Initial plugin structure

---
+++ smart-reading-progress.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Smart Reading Progress
+ * Plugin URI: https://example.com
+ * Description: Adds a reading progress bar and reading time estimate to posts.
+ * Version: 1.0.0
+ * Author: Zohaib Hassan
+ * License: GPL2
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
