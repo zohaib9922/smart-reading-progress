71eecace3016960701b137f63d58f1c2eed04ec2
Zohaib Hassan <zohaibhassan@Zohaibs-MacBook-Pro.local>
2021-02-22 15:30:00 +0500

    Fixed frontend rendering and progress bar functionality

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,12 +1,24 @@
-window.addEventListener('scroll', function () {
-    const scrollTop = document.documentElement.scrollTop;
-    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+document.addEventListener('DOMContentLoaded', function () {
 
-    const progress = (scrollTop / scrollHeight) * 100;
+    window.addEventListener('scroll', function () {
 
-    const bar = document.getElementById('srp-progress-bar');
+        const scrollTop =
+            document.documentElement.scrollTop ||
+            document.body.scrollTop;
 
-    if (bar) {
-        bar.style.width = progress + '%';
-    }
+        const scrollHeight =
+            document.documentElement.scrollHeight -
+            document.documentElement.clientHeight;
+
+        const progress =
+            (scrollTop / scrollHeight) * 100;
+
+        const bar =
+            document.getElementById('srp-progress-bar');
+
+        if (bar) {
+            bar.style.width = progress + '%';
+        }
+    });
+
 });
--- smart-reading-progress.php
+++ smart-reading-progress.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Smart Reading Progress
  * Description: Adds a reading progress bar and reading time estimate to posts.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Zohaib Hassan
  * License: GPL2
  */
@@ -11,28 +11,62 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+/*
+|--------------------------------------------------------------------------
+| Load Settings Page
+|--------------------------------------------------------------------------
+*/
+
+require_once plugin_dir_path(__FILE__) . 'includes/settings-page.php';
+
+/*
+|--------------------------------------------------------------------------
+| Enqueue Assets
+|--------------------------------------------------------------------------
+*/
+
 function srp_enqueue_assets() {
+
     wp_enqueue_style(
         'srp-style',
-        plugin_dir_url(__FILE__) . 'assets/css/style.css'
+        plugin_dir_url(__FILE__) . 'assets/css/style.css',
+        array(),
+        '1.1.0'
     );
 
     wp_enqueue_script(
         'srp-script',
         plugin_dir_url(__FILE__) . 'assets/js/progress.js',
         array(),
-        false,
+        '1.1.0',
         true
     );
 }
 
 add_action('wp_enqueue_scripts', 'srp_enqueue_assets');
 
+/*
+|--------------------------------------------------------------------------
+| Render Progress Bar
+|--------------------------------------------------------------------------
+*/
+
 function srp_render_progress_bar() {
+
+    if (!is_single()) {
+        return;
+    }
+
     echo '<div id="srp-progress-bar"></div>';
 }
 
-add_action('wp_footer', 'srp_render_progress_bar');
+add_action('wp_body_open', 'srp_render_progress_bar');
+
+/*
+|--------------------------------------------------------------------------
+| Reading Time
+|--------------------------------------------------------------------------
+*/
 
 function srp_add_reading_time($content) {
 
@@ -44,14 +78,23 @@ function srp_add_reading_time($content) {
 
     $reading_time = ceil($word_count / 200);
 
-    $reading_html = '<p class="srp-reading-time">⏱ Estimated Reading Time: ' . $reading_time . ' min</p>';
+    $reading_html =
+        '<p class="srp-reading-time">
+            ⏱ Estimated Reading Time: ' .
+            esc_html($reading_time) .
+        ' min
+        </p>';
 
     return $reading_html . $content;
 }
 
 add_filter('the_content', 'srp_add_reading_time');
 
-require_once plugin_dir_path(__FILE__) . 'includes/settings-page.php';
+/*
+|--------------------------------------------------------------------------
+| Dynamic Styles
+|--------------------------------------------------------------------------
+*/
 
 function srp_custom_styles() {
 
@@ -59,7 +102,7 @@ function srp_custom_styles() {
 
     echo '<style>
         #srp-progress-bar {
-            background:' . esc_attr($color) . ';
+            background: ' . esc_attr($color) . ';
         }
     </style>';
 }
